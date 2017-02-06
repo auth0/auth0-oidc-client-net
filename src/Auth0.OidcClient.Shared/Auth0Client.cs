f7492ba006ee12d38139751ca8fa76f17ff03aa3
Jerrie Pelser <jerrie@jerriepelser.com>
2017-02-06 11:49:48 +0700

    Preliminary WinForms version working

---
+++ src/Auth0.OidcClient.Shared/Auth0Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel.OidcClient;
+
+namespace Auth0.OidcClient
+{
+    public class Auth0Client
+    {
+        private readonly string _domain;
+        private readonly string _clientId;
+
+        public Auth0Client(string domain, string clientId)
+        {
+            _domain = domain;
+            _clientId = clientId;
+        }
+
+        public async Task LoginAsync()
+        {
+            var authority = $"https://{_domain}";
+
+            var options = new OidcClientOptions
+            {
+                Authority = authority,
+                ClientId = _clientId,
+                Scope = "openid profile",
+                RedirectUri = $"https://{_domain}/mobile",
+                Browser = new PlatformWebView(),
+                Flow = OidcClientOptions.AuthenticationFlow.Hybrid,
+                ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect
+            };
+            options.Policy.RequireAuthorizationCodeHash = false;
+            options.Policy.RequireAccessTokenHash = false;
+
+            var oidcClient = new IdentityModel.OidcClient.OidcClient(options);
+
+            var result = await oidcClient.LoginAsync();
+        }
+    }
+}
---
+++ test/WindowsFormsTestApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Auth0.OidcClient;
+
+namespace WindowsFormsTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new Auth0Client("jerrie.auth0.com", "vV9twaySQzfGesS9Qs6gOgqDsYDdgoKE");
+
+            await client.LoginAsync();
+        }
+    }
+}
