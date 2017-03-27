297ac703c8c93641aaba7f8e567c487b1253050d
Jerrie Pelser <jerrie@jerriepelser.com>
2017-03-27 12:00:38 +0700

    Working Xamarin test client

---
+++ test/XamariniOSTestApp/MyViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamariniOSTestApp
+{
+	public partial class MyViewController : UIViewController
+	{
+		public MyViewController() : base("MyViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
