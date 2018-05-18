0e12272f48e49c339d55e74f1756da041c1edf30
Jerrie Pelser <jerrie@jerriepelser.com>
2018-05-18 08:44:38 +0700

    Update to CurrentActivity 2.x

--- test/XamarinAndroidTestApp/MainApplication.cs
+++ test/XamarinAndroidTestApp/MainApplication.cs
@@ -1,63 +1,24 @@
-using System;
-
+﻿using System;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using Plugin.CurrentActivity;
 
 namespace XamarinAndroidTestApp
 {
-	//You can specify additional application information in this attribute
     [Application]
-    public class MainApplication : Application, Application.IActivityLifecycleCallbacks
+    public class MainApplication : Application
     {
         public MainApplication(IntPtr handle, JniHandleOwnership transer)
-          :base(handle, transer)
+          : base(handle, transer)
         {
         }
 
         public override void OnCreate()
         {
             base.OnCreate();
-            RegisterActivityLifecycleCallbacks(this);
-            //A great place to initialize Xamarin.Insights and Dependency Services!
-        }
-
-        public override void OnTerminate()
-        {
-            base.OnTerminate();
-            UnregisterActivityLifecycleCallbacks(this);
-        }
-
-        public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
-        {
-            CrossCurrentActivity.Current.Activity = activity;
-        }
-
-        public void OnActivityDestroyed(Activity activity)
-        {
-        }
-
-        public void OnActivityPaused(Activity activity)
-        {
-        }
-
-        public void OnActivityResumed(Activity activity)
-        {
-            CrossCurrentActivity.Current.Activity = activity;
-        }
-
-        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
-        {
-        }
-
-        public void OnActivityStarted(Activity activity)
-        {
-            CrossCurrentActivity.Current.Activity = activity;
-        }
-
-        public void OnActivityStopped(Activity activity)
-        {
+            CrossCurrentActivity.Current.Init(this);
         }
     }
 }
