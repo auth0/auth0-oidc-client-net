8b016f78b359cb928f76dc2c8410f38b33956401
Jerrie Pelser <jerrie@jerriepelser.com>
2018-05-18 11:21:50 +0700

    Change Android test app to use LoginAsync

--- test/XamarinAndroidTestApp/MainActivity.cs
+++ test/XamarinAndroidTestApp/MainActivity.cs
@@ -22,65 +22,12 @@ namespace XamarinAndroidTestApp
         private Auth0Client _client;
         private Button _loginButton;
         private TextView _userDetailsTextView;
-        //private bool _authenticating = false;
-        private AuthorizeState authorizeState;
 
-        protected override async void OnNewIntent(Intent intent)
+        protected override void OnNewIntent(Intent intent)
         {
             base.OnNewIntent(intent);
 
-            var loginResult = await _client.ProcessResponseAsync(intent.DataString, authorizeState);
-
-            var sb = new StringBuilder();
-            if (loginResult.IsError)
-            {
-                sb.AppendLine($"An error occurred during login: {loginResult.Error}");
-            }
-            else
-            {
-                sb.AppendLine($"ID Token: {loginResult.IdentityToken}");
-                sb.AppendLine($"Access Token: {loginResult.AccessToken}");
-                sb.AppendLine($"Refresh Token: {loginResult.RefreshToken}");
-
-                sb.AppendLine();
-
-                sb.AppendLine("-- Claims --");
-                foreach (var claim in loginResult.User.Claims)
-                {
-                    sb.AppendLine($"{claim.Type} = {claim.Value}");
-                }
-            }
-
-            _userDetailsTextView.Text = sb.ToString();
-            /*
-            // If we are receiving an intent and we are busy authenticating, then we need to close the authentication
-            // loop by Send-ing the data string of the intent with the ActivityMediator. 
-            // The internal process is that the DataString will contain a code which the Auth0 OIDC client will exchange 
-            // for the tokens. It will also complete the task which was await-ed when you called LoginAsync() so your code
-            // will resume from that point on
-            if (_authenticating)
-            {
-                _authenticating = false;
-                ActivityMediator.Instance.Send(intent.DataString);
-            }
-            */
-        }
-
-        protected override void OnResume()
-        {
-            base.OnResume();
-
-            /*
-            // If we are resuming and the _authenticating flag is still set, it means that the intent was not fired, so 
-            // possible the user pressed the Back button. In this case we have to cancel the authentication process
-            // by calling the Cancel() method on the ActivityMediator. This will also complete the task which was await-ed when 
-            // you called LoginAsync() so your code will resume from that point on.
-            if (_authenticating)
-            {
-                _authenticating = false;
-                ActivityMediator.Instance.Cancel();
-            }
-            */
+            ActivityMediator.Instance.Send(intent.DataString);
         }
 
         protected override void OnCreate(Bundle bundle)
@@ -106,32 +53,30 @@ namespace XamarinAndroidTestApp
         {
             _userDetailsTextView.Text = "";
 
-            //// Set a flag we're authenticating
-            //_authenticating = true;
+            // Call the login method
+            var loginResult = await _client.LoginAsync();
 
-            //// Call the login method
-            //var loginResult = await _client.LoginAsync();
+            var sb = new StringBuilder();
+            if (loginResult.IsError)
+            {
+                sb.AppendLine($"An error occurred during login: {loginResult.Error}");
+            }
+            else
+            {
+                sb.AppendLine($"ID Token: {loginResult.IdentityToken}");
+                sb.AppendLine($"Access Token: {loginResult.AccessToken}");
+                sb.AppendLine($"Refresh Token: {loginResult.RefreshToken}");
 
-            authorizeState = await _client.PrepareLoginAsync();
+                sb.AppendLine();
 
-            var uri = Android.Net.Uri.Parse(authorizeState.StartUrl);
-            var intent = new Intent(Intent.ActionView, uri);
-            intent.AddFlags(ActivityFlags.NoHistory);
-            StartActivity(intent);
+                sb.AppendLine("-- Claims --");
+                foreach (var claim in loginResult.User.Claims)
+                {
+                    sb.AppendLine($"{claim.Type} = {claim.Value}");
+                }
+            }
 
-            //---
-            //var customTabs = new CustomTabsActivityManager(this);
-
-            //// build custom tab
-            //var builder = new CustomTabsIntent.Builder(customTabs.Session)
-            //   .SetToolbarColor(Color.Argb(255, 52, 152, 219))
-            //   .SetShowTitle(true)
-            //   .EnableUrlBarHiding();
-
-            //var customTabsIntent = builder.Build();
-            //customTabsIntent.Intent.AddFlags(ActivityFlags.NoHistory);
-
-            //customTabsIntent.LaunchUrl(this, Android.Net.Uri.Parse(authorizeState.StartUrl));
+            _userDetailsTextView.Text = sb.ToString();
         }
     }
 }
