85e4474ec6e12697baeb41e2f6f0ec76765bd6ae
Damien Guard <damieng@gmail.com>
2019-05-08 15:53:13 -0700

    Renamed test app folders to match platforms

---
+++ test/iOS/MyViewController.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace XamariniOSTestApp
+{
+    [Register ("MyViewController")]
+    partial class MyViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LogoutButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView UserDetailsTextView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LoginButton != null) {
+                LoginButton.Dispose ();
+                LoginButton = null;
+            }
+
+            if (LogoutButton != null) {
+                LogoutButton.Dispose ();
+                LogoutButton = null;
+            }
+
+            if (UserDetailsTextView != null) {
+                UserDetailsTextView.Dispose ();
+                UserDetailsTextView = null;
+            }
+        }
+    }
+}
