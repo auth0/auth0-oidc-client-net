2a2b3d279a8a1ed6254d124007cdc0645391fb1b
Damien Guard <damieng@gmail.com>
2019-05-10 14:38:25 -0700

    Check in an unsigned key for building test app.

--- test/UWP/Properties/AssemblyInfo.cs
+++ test/UWP/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UwpSampleApp")]
+[assembly: AssemblyTitle("UwpTestApp")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
