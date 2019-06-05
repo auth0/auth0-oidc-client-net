e40d10f87e0ec125f6a82edef193148994089530
Damien Guard <damieng@gmail.com>
2019-06-05 11:25:18 -0700

    Unify output for Windows test apps.

--- test/UWP/MainPage.xaml.cs
+++ test/UWP/MainPage.xaml.cs
@@ -2,94 +2,91 @@
 using Windows.UI.Xaml.Controls;
 using System.Text;
 using Auth0.OidcClient;
+using System;
 
 namespace UWPTestApp
 {
     public sealed partial class MainPage : Page
     {
         private readonly Auth0Client _auth0Client;
+        private Action<string> writeLine;
+        private Action clearText;
         private string accessToken;
 
         public MainPage()
         {
             InitializeComponent();
+            writeLine = (s) => resultTextBox.Text += s + "\r\n";
+            clearText = () => resultTextBox.Text = "";
 
             _auth0Client = new Auth0Client(new Auth0ClientOptions
             {
                 Domain = "auth0-dotnet-integration-tests.auth0.com",
-                ClientId = "qmss9A66stPWTOXjR6X1OeA0DLadoNP2"
+                ClientId = "qmss9A66stPWTOXjR6X1OeA0DLadoNP2",
+                Scope = "openid profile email"
             });
         }
 
         private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            resultTextBox.Text = "Logging in...";
+            clearText();
+            writeLine("Starting login...");
 
-            var loginResult = await _auth0Client.LoginAsync();
+            var loginResult = await _auth0Client.LoginAsync(null);
 
-            // Display error
             if (loginResult.IsError)
             {
-                resultTextBox.Text += "\n" + loginResult.Error;
+                writeLine($"An error occurred during login: {loginResult.Error}");
                 return;
             }
 
             accessToken = loginResult.AccessToken;
 
-            // Display result
-            var sb = new StringBuilder();
+            writeLine($"id_token: {loginResult.IdentityToken}");
+            writeLine($"access_token: {loginResult.AccessToken}");
+            writeLine($"refresh_token: {loginResult.RefreshToken}");
 
-            sb.AppendLine("Tokens");
-            sb.AppendLine("------");
-            sb.AppendLine($"id_token: {loginResult.IdentityToken}");
-            sb.AppendLine($"access_token: {loginResult.AccessToken}");
-            sb.AppendLine($"refresh_token: {loginResult.RefreshToken}");
-            sb.AppendLine();
+            writeLine($"name: {loginResult.User.FindFirst(c => c.Type == "name")?.Value}");
+            writeLine($"email: {loginResult.User.FindFirst(c => c.Type == "email")?.Value}");
 
-            sb.AppendLine("Claims");
-            sb.AppendLine("------");
             foreach (var claim in loginResult.User.Claims)
-                sb.AppendLine($"{claim.Type}: {claim.Value}");
-
-            resultTextBox.Text += "\n" + sb.ToString();
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
+            }
         }
 
         private async void LogoutButton_OnClick(object sender, RoutedEventArgs e)
         {
-            resultTextBox.Text = "Logging out...";
+            clearText();
+            writeLine("Starting logout...");
+
             var result = await _auth0Client.LogoutAsync();
             accessToken = null;
-            resultTextBox.Text += "\n" + result.ToString();
+            writeLine(result.ToString());
         }
 
         private async void UserInfoButton_Click(object sender, RoutedEventArgs e)
         {
-            resultTextBox.Text = "Getting user info...";
+            clearText();
+
             if (string.IsNullOrEmpty(accessToken))
             {
-                resultTextBox.Text += "\n" + "You need to be logged in to get user info";
+                writeLine("You need to be logged in to get user info");
+                return;
             }
-            else
+
+            writeLine("Getting user info...");
+            var userInfoResult = await _auth0Client.GetUserInfoAsync(accessToken);
+
+            if (userInfoResult.IsError)
             {
-                var userInfoResult = await _auth0Client.GetUserInfoAsync(accessToken);
+                writeLine($"An error occurred getting user info: {userInfoResult.Error}");
+                return;
+            }
 
-                if (userInfoResult.IsError)
-                {
-                    resultTextBox.Text += "\n" + $"An error occurred getting user info: {userInfoResult.Error}";
-                }
-                else
-                {
-                    var sb = new StringBuilder();
-                    sb.AppendLine("Claims");
-                    sb.AppendLine("------");
-
-                    foreach (var claim in userInfoResult.Claims)
-                    {
-                        sb.AppendLine($"{claim.Type} = {claim.Value}");
-                    }
-
-                    resultTextBox.Text += "\n" + sb.ToString();
-                }
+            foreach (var claim in userInfoResult.Claims)
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
             }
         }
     }
--- test/WPF/MainWindow.xaml.cs
+++ test/WPF/MainWindow.xaml.cs
@@ -8,12 +8,14 @@ namespace WpfTestApp
     {
         private readonly Auth0Client _auth0Client;
         private readonly Action<string> writeLine;
+        private Action clearText;
         private string accessToken;
 
         public MainWindow()
         {
             InitializeComponent();
             writeLine = (text) => outputText.Text += text + "\n";
+            clearText = () => outputText.Text = "";
 
             _auth0Client = new Auth0Client(new Auth0ClientOptions
             {
@@ -25,66 +27,65 @@ namespace WpfTestApp
 
         private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            outputText.Text = "";
-            writeLine("Logging in...");
-            var loginResult = await _auth0Client.LoginAsync();
+            clearText();
+            writeLine("Starting login...");
+
+            var loginResult = await _auth0Client.LoginAsync(null);
 
             if (loginResult.IsError)
             {
                 writeLine($"An error occurred during login: {loginResult.Error}");
+                return;
             }
-            else
+
+            accessToken = loginResult.AccessToken;
+
+            writeLine($"id_token: {loginResult.IdentityToken}");
+            writeLine($"access_token: {loginResult.AccessToken}");
+            writeLine($"refresh_token: {loginResult.RefreshToken}");
+
+            writeLine($"name: {loginResult.User.FindFirst(c => c.Type == "name")?.Value}");
+            writeLine($"email: {loginResult.User.FindFirst(c => c.Type == "email")?.Value}");
+
+            foreach (var claim in loginResult.User.Claims)
             {
-                accessToken = loginResult.AccessToken;
-
-                writeLine($"id_token: {loginResult.IdentityToken}");
-                writeLine($"access_token: {loginResult.AccessToken}");
-                writeLine($"refresh_token: {loginResult.RefreshToken}");
-
-                writeLine($"name: {loginResult.User.FindFirst(c => c.Type == "name")?.Value}");
-                writeLine($"email: {loginResult.User.FindFirst(c => c.Type == "email")?.Value}");
-
-                foreach (var claim in loginResult.User.Claims)
-                {
-                    writeLine($"{claim.Type} = {claim.Value}");
-                }
+                writeLine($"{claim.Type} = {claim.Value}");
             }
         }
 
         private async void LogoutButton_OnClick(object sender, RoutedEventArgs e)
         {
-            outputText.Text = "";
-            writeLine("Logging out...");
+            clearText();
+            writeLine("Starting logout...");
 
-            var logoutResult = await _auth0Client.LogoutAsync();
+            var result = await _auth0Client.LogoutAsync();
             accessToken = null;
-            writeLine(logoutResult.ToString());            
+            writeLine(result.ToString());
         }
 
         private async void UserInfoButton_Click(object sender, RoutedEventArgs e)
         {
-            outputText.Text = "";
-            writeLine("Getting user info...");
+            clearText();
+
             if (string.IsNullOrEmpty(accessToken))
             {
                 writeLine("You need to be logged in to get user info");
+                return;
             }
-            else
-            {
-                var userInfoResult = await _auth0Client.GetUserInfoAsync(accessToken);
 
-                if (userInfoResult.IsError)
-                {
-                    writeLine($"An error occurred getting user info: {userInfoResult.Error}");
-                }
-                else
-                {
-                    foreach (var claim in userInfoResult.Claims)
-                    {
-                        writeLine($"{claim.Type} = {claim.Value}");
-                    }
-                }
-            }            
+            writeLine("Getting user info...");
+            var userInfoResult = await _auth0Client.GetUserInfoAsync(accessToken);
+
+            if (userInfoResult.IsError)
+            {
+                writeLine($"An error occurred getting user info: {userInfoResult.Error}");
+                return;
+            }
+
+            foreach (var claim in userInfoResult.Claims)
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
+            }
         }
     }
 }
