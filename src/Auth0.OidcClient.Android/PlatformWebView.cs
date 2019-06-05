727299979aed3e76c76833cc7069edb7bbe324a4
Damien Guard <damieng@gmail.com>
2019-06-05 12:58:00 -0700

    Provide a PlatformWebView for back-compat that defaults to best.

---
+++ src/Auth0.OidcClient.Android/PlatformWebView.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth0.OidcClient
+{
+    /// <summary>
+    /// PlatformWebView offers the best platform-specific web view implementation
+    /// at a given time. On Android that is the Chrome Custom Tabs at this time.
+    /// </summary>
+    public class PlatformWebView : ChromeCustomTabsWebView
+    {
+    }
+}
