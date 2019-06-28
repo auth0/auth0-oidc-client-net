ee12430f91d3b67922a25ffe51547bf405ddeb46
Damien Guard <damieng@gmail.com>
2019-06-28 13:23:35 -0700

    Remove PlatformWebView from iOS and Android.

--- src/Auth0.OidcClient.Android/PlatformWebView.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Auth0.OidcClient
-{
-    [Obsolete("It is recommended you leave Browser unassigned to accept the library default of ChromeCustomTabsBrowser.")]
-    public class PlatformWebView : SystemBrowser
-    {
-    }
-}
--- src/Auth0.OidcClient.iOS/PlatformWebView.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Auth0.OidcClient
-{
-    [Obsolete("It is recommended you leave Browser unassigned to accept the library default of AutoSelectBrowser.")]
-    public class PlatformWebView : AutoSelectBrowser
-    {
-    }
-}
