3bf55c720a26564334d67810ecff8e1f0bf037d6
Damien Guard <damieng@gmail.com>
2019-07-17 15:45:06 -0700

    New AutoSelectBrowser class for default best behavior capturing view context

---
+++ src/Auth0.OidcClient.Android/AutoSelectBrowser.cs
@@ -0,0 +1,18 @@
+﻿using Android.Content;
+
+namespace Auth0.OidcClient
+{
+    /// <summary>
+    /// Implement recommended browser integration for this platform.
+    /// </summary>
+    public class AutoSelectBrowser : ChromeCustomTabsBrowser
+    {
+        public AutoSelectBrowser(Context context) : base(context)
+        {
+        }
+
+        internal AutoSelectBrowser() : base(null)
+        {
+        }
+    }
+}
