6a4608e3b8c27a67680cbe237a6c86e5f4ce4fe0
Damien Guard <damieng@gmail.com>
2019-07-17 15:43:47 -0700

    Support passing view context for seamless Browser integration

--- src/Auth0.OidcClient.Android/SystemBrowser.cs
+++ src/Auth0.OidcClient.Android/SystemBrowser.cs
@@ -1,4 +1,3 @@
-using Android.App;
 using Android.Content;
 
 namespace Auth0.OidcClient
@@ -8,12 +7,20 @@ namespace Auth0.OidcClient
     /// </summary>
     public class SystemBrowser : AndroidBrowserBase
     {
-        protected override void OpenBrowser(Android.Net.Uri uri)
+        public SystemBrowser(Context context = null)
+            : base(context)
+        {
+        }
+
+        protected override void OpenBrowser(Android.Net.Uri uri, Context context = null)
         {
             var intent = new Intent(Intent.ActionView, uri);
-            intent.AddFlags(ActivityFlags.NoHistory)
-                .AddFlags(ActivityFlags.NewTask);
-            Application.Context.StartActivity(intent);
+            intent.AddFlags(ActivityFlags.NoHistory);
+
+            if (IsNewTask)
+                intent.AddFlags(ActivityFlags.NewTask);
+
+            context.StartActivity(intent);
         }
     }
 }
