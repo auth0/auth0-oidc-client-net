4a2ec3d80ce38a0ae2ecaaad08ef4bb30efd23ab
Damien Guard <damieng@gmail.com>
2019-09-30 07:42:44 -0700

    Set version to 2.4.3, add release notes

--- src/Auth0.OidcClient.WPF/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.WPF/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Windows;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.WPF")]
 [assembly: AssemblyProduct("Auth0.OidcClient.WPF")]
-[assembly: AssemblyVersion("2.4.2")]
-[assembly: AssemblyFileVersion("2.4.2")]
+[assembly: AssemblyVersion("2.4.3")]
+[assembly: AssemblyFileVersion("2.4.3")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
