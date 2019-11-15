1c3b361db08d69505ded11aecb76a328eda4441c
Damien Guard <damieng@gmail.com>
2019-11-15 13:51:22 -0800

    Remove order-judicious normalization of AssemblyInfo for core

--- src/Auth0.OidcClient.Core/AssemblyInfo.cs
+++ src/Auth0.OidcClient.Core/AssemblyInfo.cs
@@ -1,9 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Auth0.OidcClient.Core")]
-[assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
-[assembly: ComVisible(false)]
-
 [assembly: InternalsVisibleTo("Auth0.OidcClient.Core.UnitTests")]
