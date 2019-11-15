11bc04f37e7011e230136fc5b8be70488be9a633
Damien Guard <damieng@gmail.com>
2019-11-15 09:28:12 -0800

    Bump for 3.1.0

--- src/Auth0.OidcClient.Core/AssemblyInfo.cs
+++ src/Auth0.OidcClient.Core/AssemblyInfo.cs
@@ -1,3 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
 
+[assembly: AssemblyTitle("Auth0.OidcClient.Core")]
+[assembly: AssemblyProduct("Auth0.OidcClient")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: ComVisible(false)]
+
 [assembly: InternalsVisibleTo("Auth0.OidcClient.Core.UnitTests")]
--- src/Auth0.OidcClient.WinForms/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.WinForms/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.WinForms")]
-[assembly: AssemblyProduct("Auth0.OidcClient.WinForms")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyProduct("Auth0.OidcClient")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: ComVisible(false)]
