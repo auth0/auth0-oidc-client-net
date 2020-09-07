21e2a2c31425b001657dd67316720355992bc579
Damien Guard <damieng@gmail.com>
2020-09-07 13:36:33 +0100

    Update AndroidX assembly version to 3.1.6

--- src/Auth0.OidcClient.AndroidX/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.AndroidX/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.AndroidX")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.1.5")]
-[assembly: AssemblyFileVersion("3.1.5")]
+[assembly: AssemblyVersion("3.1.6")]
+[assembly: AssemblyFileVersion("3.1.6")]
 [assembly: ComVisible(false)]
