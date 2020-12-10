b9b4968118df2a459e6a9f4f1ab651c9afe9eae5
Frederik <frederik.prijck@gmail.com>
2020-12-10 15:39:00 +0100

    Add and use System Browser

--- test/WPF/MainWindow.xaml.cs
+++ test/WPF/MainWindow.xaml.cs
@@ -20,7 +20,10 @@ namespace WpfTestApp
             _auth0Client = new Auth0Client(new Auth0ClientOptions
             {
                 Domain = "auth0-dotnet-integration-tests.auth0.com",
-                ClientId = "qmss9A66stPWTOXjR6X1OeA0DLadoNP2"
+                ClientId = "qmss9A66stPWTOXjR6X1OeA0DLadoNP2",
+                Browser = new WebViewBrowser(),
+                // Browser = new SystemBrowser(),
+                // RedirectUri = "http://127.0.0.1:45656/"
             });
         }
 
