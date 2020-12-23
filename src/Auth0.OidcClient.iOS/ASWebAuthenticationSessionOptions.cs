1815dbe23a3dd4d3809987347ff7724c4fe7abdf
Frederik <frederik.prijck@gmail.com>
2020-12-23 15:12:45 +0100

    Update docs to mention SSO and consent popup

--- src/Auth0.OidcClient.iOS/ASWebAuthenticationSessionOptions.cs
+++ src/Auth0.OidcClient.iOS/ASWebAuthenticationSessionOptions.cs
@@ -8,6 +8,10 @@
         /// <summary>
         /// Specify whether or not EphemeralWebBrowserSessions should be preferred. Defaults to false.
         /// </summary>
+        /// <remarks>
+        /// Setting <see cref="PrefersEphemeralWebBrowserSession"/> to true will disable <see href="https://auth0.com/docs/sso">Single Sign On (SSO)</see> on iOS 13+.
+        /// As a consequence of that, it will also prevent from showing the popup that's being used to ask consent for using Auth0 to sign in.
+        /// </remarks>
         public bool PrefersEphemeralWebBrowserSession { get; set; }
     }
 }
