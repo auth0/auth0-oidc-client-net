26883b2c92f477ad17149f1fde8b0c351de75cdf
Frederik Prijck <frederik.prijck@auth0.com>
2021-01-07 14:40:35 +0100

    Release OidcClient.Wpf and OidcClient.iOS (#175)

--- src/Auth0.OidcClient.iOS/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.iOS/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.iOS")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.1.2")]
-[assembly: AssemblyFileVersion("3.1.2")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
 [assembly: ComVisible(false)]
