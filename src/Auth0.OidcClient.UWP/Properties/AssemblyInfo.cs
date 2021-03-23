68109120fa8a29366e78a20ea14d4b3b2a6f0e8d
Frederik Prijck <frederik.prijck@auth0.com>
2021-03-23 13:45:31 +0100

    Release Organizations (#186)

--- src/Auth0.OidcClient.UWP/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.UWP/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.UWP")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.1.3")]
-[assembly: AssemblyFileVersion("3.1.3")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
 [assembly: ComVisible(false)]
--- src/Auth0.OidcClient.WPF/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.WPF/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Windows;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.WPF")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.1.8")]
-[assembly: AssemblyFileVersion("3.1.8")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
 [assembly: ComVisible(false)]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
