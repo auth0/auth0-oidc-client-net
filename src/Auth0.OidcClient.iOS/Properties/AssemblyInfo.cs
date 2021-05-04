eba1bdc7c61474769a34abb80868a2eca7ba5d2b
Frederik Prijck <frederik.prijck@auth0.com>
2021-05-04 16:08:34 +0200

    Prepare 3.2.1 release for Core, Android, AndroidX, UWP, Winforms and WPF + 3.3.1 for iOS (#197)

--- src/Auth0.OidcClient.iOS/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.iOS/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.iOS")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.3.0")]
-[assembly: AssemblyFileVersion("3.3.0")]
+[assembly: AssemblyVersion("3.3.1")]
+[assembly: AssemblyFileVersion("3.3.1")]
 [assembly: ComVisible(false)]
