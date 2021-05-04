1471552d1ef7e5a9e057b2ee074c0c3f6507e825
Frederik <frederik.prijck@gmail.com>
2021-05-04 12:36:06 +0200

    Release Security Fixes and iOS XCode 12.5 fix

--- src/Auth0.OidcClient.AndroidX/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.AndroidX/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Auth0.OidcClient.AndroidX")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.2.0")]
-[assembly: AssemblyFileVersion("3.2.0")]
+[assembly: AssemblyVersion("3.2.1")]
+[assembly: AssemblyFileVersion("3.2.1")]
 [assembly: ComVisible(false)]
