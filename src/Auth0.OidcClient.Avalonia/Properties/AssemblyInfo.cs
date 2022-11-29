cd3e818fcd6271d3d18715ae19b1b9c2b30f2ef7
Enner Pérez <ennerperez@gmail.com>
2022-11-29 12:29:34 -0500

    AssemblyTitle Typo Fix

--- src/Auth0.OidcClient.Avalonia/Properties/AssemblyInfo.cs
+++ src/Auth0.OidcClient.Avalonia/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Auth0.OidcClient.AvaloniaUI")]
+[assembly: AssemblyTitle("Auth0.OidcClient.Avalonia")]
 [assembly: AssemblyProduct("Auth0.OidcClient")]
 [assembly: AssemblyVersion("3.2.4")]
 [assembly: AssemblyFileVersion("3.2.4")]
