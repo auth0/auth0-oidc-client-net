a03447d012742557586a0c739db8037046a77337
Enner Pérez <ennerperez@gmail.com>
2022-11-29 12:09:19 -0500

    OidcClient for Avalonia

---
+++ src/Auth0.OidcClient.Avalonia/Auth0Client.cs
@@ -0,0 +1,19 @@
+﻿namespace Auth0.OidcClient
+{
+    /// <summary>
+    /// Primary class for performing authentication and authorization operations with Auth0 using the
+    /// underlying <see cref="IdentityModel.OidcClient.OidcClient"/>.
+    /// </summary>
+    public class Auth0Client : Auth0ClientBase
+    {
+        /// <summary>
+        /// Creates a new instance of the Auth0 OIDC Client.
+        /// </summary>
+        /// <param name="options">The <see cref="Auth0ClientOptions"/> specifying the configuration for the Auth0 OIDC Client.</param>
+        public Auth0Client(Auth0ClientOptions options)
+            : base(options, "avalonia")
+        {
+            options.Browser = options.Browser ?? new WebViewBrowser();
+        }
+    }
+}
