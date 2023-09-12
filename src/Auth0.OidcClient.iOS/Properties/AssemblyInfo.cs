cb885650234b507592551b6c781d596d71ddafd1
frederikprijck <frederik.prijck@auth0.com>
2023-09-12 11:04:04 +0200

    Move all projects to SDK style

--- src/Auth0.OidcClient.iOS/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Auth0.OidcClient.iOS")]
-[assembly: AssemblyProduct("Auth0.OidcClient")]
-[assembly: AssemblyVersion("3.3.3")]
-[assembly: AssemblyFileVersion("3.3.3")]
-[assembly: ComVisible(false)]
